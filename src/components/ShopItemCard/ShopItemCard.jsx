fd2dbdc342fa6f8f4ac5b11f60e1afda44a0761d
njcushing <129661299+njcushing@users.noreply.github.com>
2023-08-25 12:38:12 +0100

    Add propTypes and deafultProps to component with isInteger validator

--- src/components/ShopItemCard/ShopItemCard.jsx
+++ src/components/ShopItemCard/ShopItemCard.jsx
@@ -1,10 +1,28 @@
-const Card = () => {
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import styles from './ShopItemCard.module.css';
 
+const isInteger = (props, propName, componentName) => {
+    if (!Number.isInteger(props[propName])) {
+        return new Error(`Invalid prop ${propName} passed to ShopItemCard. Expected an integer.`);
+    }
+}
+
+const ShopItemCard = ({
+    quantity,
+}) => {
     return (
         <>
         </>
     )
-
 };
 
-export default Card;
+ShopItemCard.propTypes = {
+    quantity: isInteger,
+}
+
+ShopItemCard.defaultProps = {
+    quantity: 0,
+}
+
+export default ShopItemCard;
