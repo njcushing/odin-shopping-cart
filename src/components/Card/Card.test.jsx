d73bcb00729f84b631a2c416e0046f1aeae49d13
njcushing <129661299+njcushing@users.noreply.github.com>
2023-08-25 12:15:44 +0100

    Create empty Card component and fix testSetup.js import

---
+++ src/components/Card/Card.test.jsx
@@ -0,0 +1,9 @@
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import Card from './Card.jsx'
+
+it('RTL setup has worked', () => {
+    expect().toBe();
+});
+
