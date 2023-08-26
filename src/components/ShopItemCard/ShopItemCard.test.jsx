f47de1eadd4b22c7fa9327754450b4a5dd77e003
njcushing <129661299+njcushing@users.noreply.github.com>
2023-08-26 10:42:38 +0100

    Write some initial tests for the quantity prop/input element

--- src/components/ShopItemCard/ShopItemCard.test.jsx
+++ src/components/ShopItemCard/ShopItemCard.test.jsx
@@ -1,9 +1,30 @@
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
-import Card from './ShopItemCard.jsx'
+import ShopItemCard from './ShopItemCard.jsx'
 
-it('RTL setup has worked', () => {
-    expect().toBe();
+describe("Prop testing...", () => {
+    describe("The 'quantity' prop...", () => {
+        test("Should have an input element rendered for it", () => {
+            render(<ShopItemCard />);
+            const quantityInput = screen.getByRole("textbox", { name: /Quantity:/i });
+            expect(quantityInput).toBeInTheDocument();
+        });
+        test("Should have a default value of 0", () => {
+            render(<ShopItemCard />);
+            const quantityInput = screen.getByRole("textbox", { name: /Quantity:/i });
+            expect(quantityInput.value).toBe("0");
+        });
+        test("Should have its value floored if it is not an integer", () => {
+            render(<ShopItemCard quantity={3.5} />);
+            const quantityInput = screen.getByRole("textbox", { name: /Quantity:/i });
+            expect(quantityInput.value).toBe("3");
+        });
+        test("Should never have a value below 0", () => {
+            render(<ShopItemCard quantity={-10} />);
+            const quantityInput = screen.getByRole("textbox", { name: /Quantity:/i });
+            expect(quantityInput.value).toBe("0");
+        });
+    });
 });
 
