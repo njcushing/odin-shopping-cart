798de57f8d214f54f274598be7d8c92c5609697e
njcushing <129661299+njcushing@users.noreply.github.com>
2023-08-26 11:40:37 +0100

    Remove 'isInteger' function from ShopItemCard - no longer needed

--- src/components/ShopItemCard/ShopItemCard.jsx
+++ src/components/ShopItemCard/ShopItemCard.jsx
@@ -2,12 +2,6 @@ import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styles from './ShopItemCard.module.css';
 
-const isInteger = (props, propName, componentName) => {
-    if (!Number.isInteger(props[propName])) {
-        return new Error(`Invalid prop ${propName} passed to ShopItemCard. Expected an integer.`);
-    }
-}
-
 const ShopItemCard = ({
     quantity,
     quantityMin,
