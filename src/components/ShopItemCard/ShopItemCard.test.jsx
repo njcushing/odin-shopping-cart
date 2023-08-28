c4c6e7951724fd3804effcef3971d22bc1ac2298
njcushing <129661299+njcushing@users.noreply.github.com>
2023-08-28 18:26:07 +0100

    Write mock for IntegerInput and test to confirm its presence in DOM

--- src/components/ShopItemCard/ShopItemCard.test.jsx
+++ src/components/ShopItemCard/ShopItemCard.test.jsx
@@ -1,3 +1,4 @@
+import { vi } from 'vitest'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
@@ -11,12 +12,27 @@ const mockItemInformation = { /* Do not change existing fields, only add new one
     quantityMax: 100,
 }
 
+vi.mock('./../IntegerInput/IntegerInput.jsx', () => ({ 
+    default: ({ label, integer }) => {
+        return (<>
+            <label>{label}
+                <input type="number" value={integer} onChange={() => {}}></input>
+            </label>
+        </>);
+    }, 
+}));
+
 describe("UI/DOM Testing...", () => {
-    describe("The 'Add to Cart' Button...", () => {
-        test("Should be rendered to the DOM", () => {
+    describe("The IntegerInput component for the 'quantity' prop...", () => {
+        test("Should have a label with textContent equal to 'Quantity:'", () => {
             render(<ShopItemCard itemInformation={mockItemInformation} />);
-            const ele = screen.getByRole("button", { name: /Add to Cart/i });
-            expect(ele).toBeInTheDocument();
+            expect(screen.getByRole("spinbutton", { name: /Quantity:/i })).toBeInTheDocument();
+        });
+    });
+    describe("The 'Add to Cart' Button...", () => {
+        test("Should have textContent equal to 'Add to Cart'", () => {
+            render(<ShopItemCard itemInformation={mockItemInformation} />);
+            expect(screen.getByRole("button", { name: /Add to Cart/i })).toBeInTheDocument();
         });
     });
 });
