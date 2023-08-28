f5553589021ce2d83947cf9159a85c9dcc8c58aa
njcushing <129661299+njcushing@users.noreply.github.com>
2023-08-28 17:37:03 +0100

    Write test to ensure Add to Cart button is rendering

--- src/components/ShopItemCard/ShopItemCard.test.jsx
+++ src/components/ShopItemCard/ShopItemCard.test.jsx
@@ -3,6 +3,22 @@ import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import ShopItemCard from './ShopItemCard.jsx'
 
-test("", () => {});
+const mockItemInformation = { /* Do not change existing fields, only add new ones */
+    name: "Shop Item",
+    originalPrice: 1500,
+    currentPrice: 1500,
+    quantityMin: 0,
+    quantityMax: 100,
+}
+
+describe("UI/DOM Testing...", () => {
+    describe("The 'Add to Cart' Button...", () => {
+        test("Should be rendered to the DOM", () => {
+            render(<ShopItemCard itemInformation={mockItemInformation} />);
+            const ele = screen.getByRole("button", { name: /Add to Cart/i });
+            expect(ele).toBeInTheDocument();
+        });
+    });
+});
 
 
