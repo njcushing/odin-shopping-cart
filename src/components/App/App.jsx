0f7578cb4086b4ef1d6faa6a58086cd3a953c956
njcushing <129661299+njcushing@users.noreply.github.com>
2023-08-29 19:51:18 +0100

    Display component in browser to see it properly

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import { v4 as uuidv4 } from 'uuid';
 
 import ShopItemCard from '../ShopItemCard/ShopItemCard';
+
+const mockItems = [
+    {
+        id: uuidv4(),
+        name: "Navy Backpack",
+        imageUrl: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+        originalPrice: 1299,
+        currentPrice: 849,
+        quantityMin: 0,
+        quantityMax: 100,
+    },
+];
 
 function App() {
     return (
         <>
-            <ShopItemCard
-                itemInformation={{
-                    quantityMin: 0,
-                    quantityMax: 100,
-                }}
-            />
+            {mockItems.map((item) => 
+                <ShopItemCard itemInformation={item} key={item.id} />
+            )}
         </>
     )
 }
