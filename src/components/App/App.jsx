14d444f334e7887062a11049123bfa564dccadb6
njcushing <129661299+njcushing@users.noreply.github.com>
2023-08-30 10:16:23 +0100

    Write fetch function to Fake Store API

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,54 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react'
+import styles from './App.module.css'
 import { v4 as uuidv4 } from 'uuid';
 
 import ShopItemCard from '../ShopItemCard/ShopItemCard';
 
-const mockItems = [
-    {
+const Url = 'https://fakestoreapi.com/products';
+const addItemFromAPI = (array, item) => {
+    array.push({
         id: uuidv4(),
-        name: "Navy Backpack",
-        imageUrl: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-        originalPrice: 1299,
-        currentPrice: 849,
+        name: item.description,
+        imageUrl: item.image,
+        originalPrice: (item.price * 100),
+        currentPrice: Math.ceil((item.price * 100) - Math.random((item.price * 100) / 3)),
         quantityMin: 0,
-        quantityMax: 100,
-    },
-];
+        quantityMax: item.rating.count,
+    })
+}
 
 function App() {
+    const [category, setCategory] = useState("men's clothing");
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        let itemsNew = [];
+        (() => {
+            fetch(Url, { mode: 'cors' })
+            .then((response) => {
+                if (response.status >= 400) {
+                    throw new Error(`Warning: ${Url} is an invalid call to the Fake Store API`);
+                }
+                return response.json();
+            })
+            .then((response) => {
+                response.forEach((item) => {
+                    if (item.category === category) {
+                        addItemFromAPI(itemsNew, item);
+                    }
+                });
+                setItems(itemsNew);
+            })
+            .catch((error) => { throw new Error(error) });
+        })();
+    }, [category]);
+
     return (
-        <>
-            {mockItems.map((item) => 
+        <div className={styles["item-list"]}>
+            {items.map((item) => 
                 <ShopItemCard itemInformation={item} key={item.id} />
             )}
-        </>
+        </div>
     )
 }
 
