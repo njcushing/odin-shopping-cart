0acf76a9e204007dafc7348c48b456dfbfa4f502
njcushing <129661299+njcushing@users.noreply.github.com>
2023-08-30 11:05:55 +0100

    Set PropTypes for NavBar.Component props

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,37 @@
 import { useState, useEffect } from 'react'
+import PropTypes from 'prop-types';
 import styles from './NavBar.module.css'
 import { v4 as uuidv4 } from 'uuid';
 
-const NavBar = (props) => {
-    return;
+const option = () => {
+    return {
+        text: "",
+        textColour: "black",
+        backgroundColour: "white",
+    }
+}
+
+const Component = ({
+    options,
+    defaultOption,
+}) => {
+    const [currentOption, setCurrentOption] = useState("");
+
+    return (
+        <>
+        <nav className={styles["NavBar"]}>
+        </nav>
+        </>
+    );
 };
 
-NavBar.propTypes = {}
+Component.propTypes = {
+    options: PropTypes.arrayOf(PropTypes.shape({
+        text: PropTypes.string,
+        textColour: PropTypes.string,
+        backgroundColour: PropTypes.string,
+    })).isRequired,
+    defaultOption: PropTypes.string,
+}
 
-NavBar.defaultProps = {}
-
-export default NavBar;
+export { Component, option };
