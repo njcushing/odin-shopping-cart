79575a175772920c913353fc8740d7da5e4375c8
njcushing <129661299+njcushing@users.noreply.github.com>
2023-08-30 10:37:48 +0100

    Create boilerplate files for new NavBar component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import styles from './NavBar.module.css'
+import { v4 as uuidv4 } from 'uuid';
+
+const NavBar = (props) => {
+    return;
+};
+
+NavBar.propTypes = {}
+
+NavBar.defaultProps = {}
+
+export default NavBar;
---
+++ src/components/NavBar/NavBar.test.jsx
@@ -0,0 +1,7 @@
+import { vi } from 'vitest'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import NavBar from './NavBar.jsx'
+
+test.only("Placeholder", () => {});
