df2ccefb096f025a32a3b9598939ea7cd8bae933
njcushing <129661299+njcushing@users.noreply.github.com>
2023-08-30 11:01:59 +0100

    Write test for NavBar - check number of children

--- src/components/NavBar/NavBar.test.jsx
+++ src/components/NavBar/NavBar.test.jsx
@@ -2,6 +2,25 @@ import { vi } from 'vitest'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
-import NavBar from './NavBar.jsx'
+import * as NavBar from './NavBar.jsx'
 
-test.only("Placeholder", () => {});
+console.log(NavBar);
+
+const newOption = NavBar.option();
+const mockOptions = [
+    { ...newOption, text: "Option 1", },
+    { ...newOption, text: "Option 2", },
+    { ...newOption, text: "Option 3", },
+    { ...newOption, text: "Option 4", },
+    { ...newOption, text: "Option 5", },
+]
+
+describe("UI/DOM testing...", () => {
+    describe("The NavBar button container...", () => {
+        test("Should contain the same number of buttons as options provided", () => {
+            render(<NavBar.Component options={mockOptions} defaultOption="Option 1" />);
+            const ele = screen.getByRole("navigation");
+            expect(ele.children.length).toBe(5);
+        })
+    });
+});
