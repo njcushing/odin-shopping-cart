243070b7adcc0a2ee762d8ba5dece08a71e6342b
njcushing <129661299+njcushing@users.noreply.github.com>
2023-09-09 12:44:57 +0100

    Write test to validate items being added to list

--- src/components/ShopCart/ShopCart.test.jsx
+++ src/components/ShopCart/ShopCart.test.jsx
@@ -3,3 +3,25 @@ import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import * as ShopCart from './ShopCart.jsx'
+
+const mockItems = [
+    { ...ShopCart.item(), name: "Item 1", },
+    { ...ShopCart.item(), name: "Item 2", },
+    { ...ShopCart.item(), name: "Item 3", },
+    { ...ShopCart.item(), name: "Item 4", },
+    { ...ShopCart.item(), name: "Item 5", },
+]
+
+describe("UI/DOM testing...", () => {
+    describe("The unordered-list container element containing the shop items...", () => {
+        test("Should contain the same number of children as items in the cart", () => {
+            render(<ShopCart.Component
+                ariaLabel={"shop-cart"}
+                items={mockItems}
+            />);
+
+            const ele = screen.getByRole("list", { name: /shop-cart/i });
+            expect(ele.children.length).toBe(5);
+        });
+    });
+});
