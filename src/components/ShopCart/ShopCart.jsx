1fd9b2ca2a622cd84ae4d1004cc38fcbdf77a3a7
njcushing <129661299+njcushing@users.noreply.github.com>
2023-09-09 12:45:32 +0100

    Pass current tests for ShopCart component

--- src/components/ShopCart/ShopCart.jsx
+++ src/components/ShopCart/ShopCart.jsx
@@ -3,12 +3,41 @@ import PropTypes from 'prop-types';
 import styles from './ShopCart.module.css'
 import { v4 as uuidv4 } from 'uuid';
 
-const Component = () => {
-
+const item = () => {
+    return {
+        id: uuidv4(),
+        name: "",
+    }
 }
 
-Component.propTypes = {}
+const Component = ({
+    ariaLabel,
+    items,
+}) => {
+    return (
+        <>
+        <ul className={styles["ShopCart"]} aria-label={ariaLabel}>
+        {items.map((item) => 
+            <li
+                key={item.id}    
+            ></li>
+        )}
+        </ul>
+        </>
+    )
+}
 
-Component.defaultProps = {}
+Component.propTypes = {
+    ariaLabel: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string,
+        name: PropTypes.string,
+    })),
+}
 
-export { Component };
+Component.defaultProps = {
+    ariaLabel: "shop-cart",
+    items: {},
+}
+
+export { Component, item };
