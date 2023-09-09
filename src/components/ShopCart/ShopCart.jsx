b533ebf82a9d0817affeaf81819e962cc46e1b19
njcushing <129661299+njcushing@users.noreply.github.com>
2023-09-09 12:25:06 +0100

    Set up boilerplate files for new ShopCart component

---
+++ src/components/ShopCart/ShopCart.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+import PropTypes from 'prop-types';
+import styles from './ShopCart.module.css'
+import { v4 as uuidv4 } from 'uuid';
+
+const Component = () => {
+
+}
+
+Component.propTypes = {}
+
+Component.defaultProps = {}
+
+export { Component };
