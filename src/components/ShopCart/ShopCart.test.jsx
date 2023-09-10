1bc63ea4ebf7fb788d5557b1a5cb7207c75e096b
njcushing <129661299+njcushing@users.noreply.github.com>
2023-09-10 10:49:22 +0100

    Write test for checking empty cart heading exists

--- src/components/ShopCart/ShopCart.test.jsx
+++ src/components/ShopCart/ShopCart.test.jsx
@@ -14,14 +14,26 @@ const mockItems = [
 
 describe("UI/DOM testing...", () => {
     describe("The unordered-list container element containing the shop items...", () => {
-        test("Should contain the same number of children as items in the cart", () => {
-            render(<ShopCart.Component
-                ariaLabel={"shop-cart"}
-                items={mockItems}
-            />);
+        describe("If there are items in the cart...", () => {
+            test("Should contain the same number of children as items in the cart", () => {
+                render(<ShopCart.Component
+                    ariaLabel={"shop-cart"}
+                    items={mockItems}
+                />);
 
-            const ele = screen.getByRole("list", { name: /cart-item-list/i });
-            expect(ele.children.length).toBe(5);
+                const ele = screen.getByRole("list", { name: /cart-item-list/i });
+                expect(ele.children.length).toBe(5);
+            });
+        });
+        describe("If the cart is empty...", () => {
+            test("Should contain a heading that indicates the empty state of the cart", () => {
+                render(<ShopCart.Component
+                    ariaLabel={"shop-cart"}
+                    items={{}}
+                />);
+
+                expect(screen.getByRole("heading", { name: /Your Cart is Empty/i })).toBeInTheDocument();
+            });
         });
     });
 });
