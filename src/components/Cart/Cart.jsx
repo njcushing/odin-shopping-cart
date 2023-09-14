f43e05428d58cb852d2deebe11ba33bca6319de9
njcushing <129661299+njcushing@users.noreply.github.com>
2023-09-14 10:34:24 +0100

    Add container and title elements to Cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,9 +3,13 @@ import PropTypes from 'prop-types';
 import styles from './Cart.module.css';
 
 const Cart = () => {
-    return (<>
-
-    </>)
+    return (
+        <div className={styles["Cart"]} >
+            <div className={styles["container"]}>
+                <h1 className={styles["title"]}>Your Items</h1>
+            </div>
+        </div>
+    )
 }
 
 Cart.propTypes = {}
