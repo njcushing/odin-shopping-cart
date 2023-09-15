003a42a88e1ee9c7ba89da15c98cefc9ba6c8c20
njcushing <129661299+njcushing@users.noreply.github.com>
2023-09-15 12:25:39 +0100

    Fix Button test suite due to refactor to use Link

--- src/components/Button/Button.test.jsx
+++ src/components/Button/Button.test.jsx
@@ -2,35 +2,36 @@ import { vi } from 'vitest'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
+import { BrowserRouter } from "react-router-dom";
 import Button from './Button.jsx'
 
 describe("UI/DOM Testing...", () => {
     describe("The button element...", () => {
         test(`Should have the same textContent as the provided 'text' prop's
          value`, () => {
-            render(<Button text="Example" />);
-            const button = screen.getByRole("button", { name: /Example/i });
+            render(<BrowserRouter><Button text="Example" /></BrowserRouter>);
+            const button = screen.getByRole("link", { name: /Example/i });
             expect(button).toBeInTheDocument();
         });
         test("When clicked, should invoke the provided callback function", async () => {
             const user = userEvent.setup();
             const callback = vi.fn();
             
-            render(<Button onClickHandler={callback} />);
-            const button = screen.getByRole("button", { name: /Button/i });
+            render(<BrowserRouter><Button onClickHandler={callback} /></BrowserRouter>);
+            const button = screen.getByRole("link", { name: /Button/i });
 
             await user.click(button);
 
             expect(callback).toHaveBeenCalled();
         });
         test("Should not have the 'disabled' attribute by default", () => {
-            render(<Button />);
-            const button = screen.getByRole("button", { name: /Button/i });
+            render(<BrowserRouter><Button /></BrowserRouter>);
+            const button = screen.getByRole("link", { name: /Button/i });
             expect(button.getAttribute('disabled')).toBeNull();
         });
         test("Should have the 'disabled' attribute if the 'enabled' prop === false", () => {
-            render(<Button enabled={false} />);
-            const button = screen.getByRole("button", { name: /Button/i });
+            render(<BrowserRouter><Button enabled={false} /></BrowserRouter>);
+            const button = screen.getByRole("link", { name: /Button/i });
             expect(button.getAttribute('disabled')).toBeFalsy();
         });
     });
