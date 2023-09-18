15cbf18a1cc9d3e31b2985b498298703927e7e73
njcushing <129661299+njcushing@users.noreply.github.com>
2023-09-18 19:12:13 +0100

    Write test for App component

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -3,12 +3,68 @@ import { render, screen, act } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import App from './App.jsx'
+import FakeStoreAPIFetch from "./../../modules/FakeStoreAPIFetch/FakeStoreAPIFetch";
 
 vi.mock('./../Button/Button.jsx', () => ({ 
     default: () => {
         return (<button></button>);
     }, 
 }));
+
+const FakeStoreAPIItemFormat = {
+    id: 0,
+    title: "",
+    price: 109.95,
+    description: "",
+    category: "",
+    image: "",
+    rating: {
+        rate: 0.0,
+        count: 1,
+    },
+};
+const mockItems = [
+    { ...FakeStoreAPIItemFormat,
+        id: 1,
+        title: "Item 1",
+        price: 1.00,
+        description: "Test Item 1",
+        category: "Test Category 1",
+        image: "http://a",
+        rating: {
+            rate: 3.5,
+            count: 120,
+        }
+    },
+    { ...FakeStoreAPIItemFormat,
+        id: 2,
+        title: "Item 2",
+        price: 2.00,
+        description: "Test Item 2",
+        category: "Test Category 2",
+        image: "http://b",
+        rating: {
+            rate: 4.0,
+            count: 80,
+        }
+    },
+    { ...FakeStoreAPIItemFormat,
+        id: 3,
+        title: "Item 3",
+        price: 3.00,
+        description: "Test Item 3",
+        category: "Test Category 3",
+        image: "http://c",
+        rating: {
+            rate: 2.0,
+            count: 40,
+        }
+    },
+];
+
+vi.mock("./../../modules/FakeStoreAPIFetch/FakeStoreAPIFetch");
+FakeStoreAPIFetch.mockImplementation(() => mockItems);
+
 
 describe("UI/DOM testing...", () => {
     describe("On the '/' route...", () => {
@@ -17,7 +73,7 @@ describe("UI/DOM testing...", () => {
             test("Should have as many children as there are categories", async () => {
                 await act(async () => { render(<App />) });
                 const ele = screen.getByRole("list", { name: "home-categories" });
-                expect(ele.children.length).toBe(0);
+                expect(ele.children.length).toBe(3);
             });
         });
     });
